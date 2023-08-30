7a27040ea9046febac94028f40db6df8e77857c6
egorkqq <egorkqq@gmail.com>
2023-08-30 17:26:49 +0300

    feat: init project structure

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { HomePage } from "./pages";
+
+const App = () => <HomePage />;
+
+export default App;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { List } from "../../components/list";
+import { Pagination } from "../../components/pagination";
+
+export const HomePage = () => {
+  return (
+    <div>
+      Test task
+      <List />
+      <Pagination />
+    </div>
+  );
+};
